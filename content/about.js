f8199d606f4771c252f7041ada58d2c405262af0
GameDevMadeEasy <jglover88@hotmail.com>
2024-05-16 17:11:43 -0500

    no message

---
+++ content/about.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('About page script loaded');
+    // Add any additional JavaScript specific to the about page here
+});
---
+++ content/home.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Home page script loaded');
+    // Add any additional JavaScript specific to the home page here
+});
